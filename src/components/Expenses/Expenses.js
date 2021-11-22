65bfbfad4e4b05439174b88ab9d6c4683fdf0ff3
aamna-sys <aamna.sayyed@systemsltd.com>
2021-11-22 13:15:47 +0500

    Update selectedYear

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import ExpenseList from "./ExpenseList";
 import ExpenseChart from "./ExpenseChart";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
